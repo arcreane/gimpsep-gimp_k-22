a336362b469f65a45b9af9f6c22e0683dfca1208
ehdustj1001 <ehdustj1001@naver.com>
2022-06-12 14:14:35 +0200

    Initial Commit

---
+++ Multimedia_FinalProject/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Multimedia_FinalProject
+//
+//  Created by DYS on 6/12/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
